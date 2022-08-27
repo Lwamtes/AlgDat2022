2ab6f83e6db26a4b8aa528d901d3ff5b886fc37d
Lwamtes <89571562+Lwamtes@users.noreply.github.com>
2022-08-27 14:52:03 +0200

    first version with skeleton code

---
+++ src/ukeOpp112.java
@@ -0,0 +1,17 @@
+public class ukeOpp112 {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        int[] a={8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+        int min_index=min(a);
+        System.out.println(min_index);
+
+    }
+    static int min(int []a){ // her vil vi definere at a er et array med tall, for data maskinen forstår ikke hva a er
+        int min_value=a[0];
+        int min_index=0;
+
+
+        return 0;
+
+    }
+}
